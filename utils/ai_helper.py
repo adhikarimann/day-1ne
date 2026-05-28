1d76e7f8c911cd40f8094b4414fda9dc0b46ab80
Manmohan Adhikari <adhikarimann27@gmail.com>
2026-05-28 22:08:18 +0530

    Prepare DAY-1NE for Streamlit deployment

--- utils/ai_helper.py
+++ utils/ai_helper.py
@@ -1,12 +1,13 @@
 from groq import Groq
 from dotenv import load_dotenv
+import streamlit as st
 import os
 from datetime import datetime
 
 load_dotenv()
 
 client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+    api_key=os.getenv("GROQ_API_KEY") or st.secrets["GROQ_API_KEY"]
 )
 
 
