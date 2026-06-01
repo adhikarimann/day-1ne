899cfb75323d263ccdec8b6ea9c77e2cd6341ae3
Manmohan Adhikari <adhikarimann27@gmail.com>
2026-06-01 21:06:45 +0530

    Revise Concepts

--- learnings/practice.py
+++ learnings/practice.py
@@ -1,7 +1,7 @@
 name = input("enter your name: ")
 subject = input("What are you studying today? ")
 hours = int(input("How many hours will you study? "))
-
+about = input("tell about yourself!")
 print(f"\nWelcome {name}")
 print(f"Today's subject: {subject}")
 print(f"Planned study hours: {hours}")
